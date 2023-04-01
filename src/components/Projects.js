96e789cd9e915729ea3e9ca61220a2138caa6906
Saanvi Bhagat <sanvibhagat13@gmail.com>
2023-04-01 11:34:58 +0530

    changess in projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,42 @@
 import React from "react";
-import Webdev from "./WebDevProj";
+const proj=[
+    {
+        name:"Calculator",
+        desc:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos, libero!",
+        imgurl:""
+    },
+    {
+        name:"To-do List",
+        desc:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos, libero!",
+        imgurl:""
+    },
+    {
+        name:"Registration form",
+        desc:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos, libero!",
+        imgurl:""
+    },
+    {
+        name:"Mass Mail Dispatcher",
+        desc:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos, libero!",
+        imgurl:""
+    },
+    {
+        name:"The Sparks Bank",
+        desc:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos, libero!",
+        imgurl:""
+    },
+    {
+        name:"Surr-Taal",
+        desc:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos, libero!",
+        imgurl:""
+    }
+
+]
+
 const Projects = () =>{
     return (
         <>
         <h2 className="proj-title">My Projects</h2>
-        <Webdev />
         </>
     )
 }
