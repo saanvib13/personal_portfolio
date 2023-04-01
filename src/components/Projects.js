50e121b800f063828825d1f8a3f683b2877c7fa3
Saanvi Bhagat <sanvibhagat13@gmail.com>
2023-04-01 14:12:17 +0530

    changes in skills section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,42 +1,128 @@
 import React from "react";
-const proj=[
+import { Container, Row, Col } from "react-bootstrap";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+const proj = [
     {
-        name:"Calculator",
-        desc:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos, libero!",
-        imgurl:""
+        name: "Calculator",
+        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos, libero!",
+        imgurl: ""
     },
     {
-        name:"To-do List",
-        desc:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos, libero!",
-        imgurl:""
+        name: "To-do List",
+        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos, libero!",
+        imgurl: ""
     },
     {
-        name:"Registration form",
-        desc:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos, libero!",
-        imgurl:""
+        name: "Registration form",
+        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos, libero!",
+        imgurl: ""
     },
     {
-        name:"Mass Mail Dispatcher",
-        desc:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos, libero!",
-        imgurl:""
+        name: "Mass Mail Dispatcher",
+        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos, libero!",
+        imgurl: ""
     },
     {
-        name:"The Sparks Bank",
-        desc:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos, libero!",
-        imgurl:""
+        name: "The Sparks Bank",
+        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos, libero!",
+        imgurl: ""
     },
     {
-        name:"Surr-Taal",
-        desc:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos, libero!",
-        imgurl:""
+        name: "Surr-Taal",
+        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos, libero!",
+        imgurl: ""
+    },
+    {
+        name: "MentalBloomer",
+        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos, libero!",
+        img: ""
     }
 
 ]
 
-const Projects = () =>{
+const Projects = () => {
+    const responsive = {
+        superLargeDesktop: {
+          // the naming can be any, depends on you.
+          breakpoint: { max: 4000, min: 3000 },
+          items: 5
+        },
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 3
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 2
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1
+        }
+      };
     return (
         <>
-        <h2 className="proj-title">My Projects</h2>
+            <section classname="project">
+                <Container>
+                    <Row>
+                        <Col>
+                            <h2 className="proj-title">My Projects</h2>
+                            {/* <Tab.Container id="project-tab">
+                            <Nav variant="pills" defaultActiveKey="first">
+                                <Nav.Item>
+                                    <Nav.Link eventKey="first">Tab One</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="second">Tab Two</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="third">
+                                        Tab Three
+                                    </Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="four">
+                                        Tab four
+                                    </Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="five">
+                                        Tab five
+                                    </Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="six">
+                                        Tab six
+                                    </Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="seven">
+                                        Tab Seven
+                                    </Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                            </Tab.Container> */}
+                            <Carousel responsive={responsive} infinite={true} className="skill-slider">
+                        <div className="item">
+                          <img src="" alt=""/>
+                          <h5>Web Development</h5>
+                        </div>
+                        <div className="item">
+                          <a href="#"><img src="" alt=""/></a>
+                          <h5>Data Science</h5>
+                        </div>
+                        <div className="item">
+                          <a href="#"><img src="" alt=""/></a>
+                          <h5>More to be added...</h5> 
+                        </div>
+                      </Carousel> 
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+
         </>
     )
 }
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,42 +2,44 @@ import { Container, Row, Col } from "react-bootstrap";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import web_dev_skill from '../assets/img/web_dev.png'
-import Webdev from "./WebDevProj";
 import data_science from '../assets/img/datascience.png'
 import add_img from '../assets/img/add_img.jpg'
+import Nav from 'react-bootstrap/Nav';
+import Tab from 'react-bootstrap/Tab';
 
-const Skills = () =>{
-    const responsive = {
-        superLargeDesktop: {
-          // the naming can be any, depends on you.
-          breakpoint: { max: 4000, min: 3000 },
-          items: 5
-        },
-        desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 3
-        },
-        tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 2
-        },
-        mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 1
-        }
-      };
-      return (
-        <section className="skill">
-            <Container>
-                <Row>
-                    <Col>
-                    <div className="skill-div">
-                      <h2>
-                        Skills
-                      </h2>
-                      <Carousel responsive={responsive} infinite={true} className="skill-slider">
+const Skills = () => {
+  // const responsive = {
+  //     superLargeDesktop: {
+  //       // the naming can be any, depends on you.
+  //       breakpoint: { max: 4000, min: 3000 },
+  //       items: 5
+  //     },
+  //     desktop: {
+  //       breakpoint: { max: 3000, min: 1024 },
+  //       items: 3
+  //     },
+  //     tablet: {
+  //       breakpoint: { max: 1024, min: 464 },
+  //       items: 2
+  //     },
+  //     mobile: {
+  //       breakpoint: { max: 464, min: 0 },
+  //       items: 1
+  //     }
+  //   };
+
+  return (
+    <section className="skill">
+      <Container>
+        <Row>
+          <Col>
+            <div className="skill-div">
+              <h2>
+                Skills
+              </h2>
+              {/* <Carousel responsive={responsive} infinite={true} className="skill-slider">
                         <div className="item">
-                          <a href={<Webdev />}><img src={web_dev_skill} alt=""/></a>
+                          <img src={web_dev_skill} alt=""/>
                           <h5>Web Development</h5>
                         </div>
                         <div className="item">
@@ -48,13 +50,47 @@ const Skills = () =>{
                           <a href="#"><img src={add_img} alt=""/></a>
                           <h5>More to be added...</h5> 
                         </div>
-                      </Carousel>
-                    </div>
-                    </Col>
-                </Row>
-            </Container>
-        </section>
-      )
+                      </Carousel> */}
+              <Tab.Container id="project-tab"  defaultActiveKey="first">
+                <Nav variant="pills" defaultActiveKey="first">
+                  <Nav.Item>
+                    <Nav.Link eventKey="first">Tab One</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link eventKey="second">Tab Two</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link eventKey="third">
+                      Tab Three
+                    </Nav.Link>
+                  </Nav.Item>
+                </Nav>
+                <Tab.Content>
+                  <Tab.Pane eventKey="first">
+                    <Row className="skill-slider">
+                    <div className="item">
+                          <img src={web_dev_skill} alt=""/>
+                          <div className="skill-desc">
+                          <h5>Web Development</h5>
+                          <p>Interned at:</p>
+                          <ul>
+                            <li>Exposys Data Labs</li>
+                            <li>LetsGrowMore</li>
+                            <li>The Sparks Foundation</li>
+                            <li>Currently, a member of Google Developers Student Club, MSIT</li>
+                          </ul>
+                          </div>
+                        </div>
+                    </Row>
+                  </Tab.Pane>
+                </Tab.Content>
+              </Tab.Container>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  )
 }
 
 export default Skills
